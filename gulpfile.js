7418aad6131d1193bfbd7e3386af5b9f774a7662
Chesco <ingfranciscoroldan@gmail.com>
2016-12-18 22:08:33 -0500

    Adding also libraries

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'), 
+watch = require('gulp-watch'), 
+postcss = require('gulp-postcss'),
+autoprefixer =require('autoprefixer'),
+cssvars=require('postcss-simple-vars'),
+nested= require('postcss-nested'),
+cssimport = require('postcss-import'),
+browserSync = require('browser-sync').create();
+
+gulp.task('default',function(){
+
+console.log("Hooray - you created a Gulp task.");
+
+});
+
+gulp.task('html',function(){
+console.log("Imaging something useful being used on your HTML file.");
+
+});
+
+gulp.task('styles',function(){
+
+return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssimport,cssvars,nested,autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+
+});
+
+gulp.task('watch',function	(){
+
+
+	browserSync.init({
+		server: {
+			baseDir:"app"
+		}
+	});
+
+	watch('./app/index.html',function(){
+
+		browserSync.reload();
+
+	});
+
+	watch('./app/assets/styles/**/*.css',function(){
+
+		gulp.start('cssInject');
+	})
+
+
+
+
+})
+
+gulp.task('cssInject',['styles'],function(){
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+
+});
